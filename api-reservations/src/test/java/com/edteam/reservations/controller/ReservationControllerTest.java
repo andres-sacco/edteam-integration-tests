951d3d975f24905ac7bbed156f90c2c87737273a
asacco <andres.sacco@travelx.io>
2024-02-26 08:57:12 -0300

    feat: final

---
+++ api-reservations/src/test/java/com/edteam/reservations/controller/ReservationControllerTest.java
@@ -0,0 +1,33 @@
+package com.edteam.reservations.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+class ReservationControllerTest {
+
+    public static final String CONTENT_TYPE = "application/json";
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    void checkGet() throws Exception {
+        int request = 1;
+
+        mockMvc.perform(get("/reservation/".concat(String.valueOf(request))).contentType(CONTENT_TYPE))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(CONTENT_TYPE))
+                .andExpect(jsonPath("$.creationDate").value("2023-11-11"));;
+    }
+}
