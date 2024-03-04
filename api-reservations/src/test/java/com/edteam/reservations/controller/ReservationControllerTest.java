a5c146b1e591939584f89f096297dbc0000789e4
asacco <andres.sacco@travelx.io>
2024-03-04 15:38:14 -0300

    final

--- api-reservations/src/test/java/com/edteam/reservations/controller/ReservationControllerTest.java
+++ api-reservations/src/test/java/com/edteam/reservations/controller/ReservationControllerTest.java
@@ -11,9 +11,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Tags(@Tag("integration"))
 @DisplayName("Check the functionality of the application")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -30,6 +31,7 @@ class ReservationControllerTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
+    @Order(1)
     @Tag("success-case")
     @DisplayName("should return the information of the reservation")
     @Test
@@ -51,9 +53,102 @@ class ReservationControllerTest {
         assertAll(
                 () -> assertEquals(200, mvcResult.getResponse().getStatus()),
                 () -> assertEquals(CONTENT_TYPE, mvcResult.getResponse().getContentType()),
-                () -> assertTrue(mvcResult.getResponse().getContentAsString().contains("2023-11-11"))
+                () -> assertEquals("{\"id\":1,\"version\":3,\"passengers\":[{\"firstName\":\"Andres\",\"lastName\":\"Sacco\",\"documentNumber\":\"AB554713\",\"documentType\":\"PASSPORT\",\"birthday\":\"1985-01-01\"},{\"firstName\":\"Andres\",\"lastName\":\"Sacco\",\"documentNumber\":\"AB554718\",\"documentType\":\"PASSPORT\",\"birthday\":\"1985-01-01\"}],\"itinerary\":{\"id\":1,\"version\":4,\"segment\":[{\"origin\":\"BUE\",\"destination\":\"MIA\",\"departure\":\"2023-12-31\",\"arrival\":\"2024-01-01\",\"carrier\":\"AA\"}],\"price\":{\"totalPrice\":30.00,\"totalTax\":20.00,\"basePrice\":10.00}},\"creationDate\":\"2023-11-11\"}", mvcResult.getResponse().getContentAsString())
         );
     }
+
+    @Order(1)
+    @Tag("success-case")
+    @DisplayName("should return the information of the list of reservations")
+    @Test
+    void get_should_return_the_list_of_reservations() throws Exception {
+
+        LOGGER.info("Running get_should_return_the_list_of_reservations");
+
+        // Given
+        String firstName = "Andres";
+
+        // When
+        MvcResult mvcResult = mockMvc.perform(get("/reservation?firstName=".concat(firstName)).contentType(CONTENT_TYPE))
+                .andReturn();
+        // Then
+        assertAll(
+                () -> assertEquals(200, mvcResult.getResponse().getStatus()),
+                () -> assertEquals(CONTENT_TYPE, mvcResult.getResponse().getContentType()),
+                () -> assertEquals("[{\"id\":1,\"version\":3,\"passengers\":[{\"firstName\":\"Andres\",\"lastName\":\"Sacco\",\"documentNumber\":\"AB554713\",\"documentType\":\"PASSPORT\",\"birthday\":\"1985-01-01\"},{\"firstName\":\"Andres\",\"lastName\":\"Sacco\",\"documentNumber\":\"AB554718\",\"documentType\":\"PASSPORT\",\"birthday\":\"1985-01-01\"}],\"itinerary\":{\"id\":1,\"version\":4,\"segment\":[{\"origin\":\"BUE\",\"destination\":\"MIA\",\"departure\":\"2023-12-31\",\"arrival\":\"2024-01-01\",\"carrier\":\"AA\"}],\"price\":{\"totalPrice\":30.00,\"totalTax\":20.00,\"basePrice\":10.00}},\"creationDate\":\"2023-11-11\"}]", mvcResult.getResponse().getContentAsString())
+        );
+    }
+
+    @Order(2)
+    @Tag("success-case")
+    @DisplayName("should return a persisted reservation")
+    @Test
+    void insert_should_return_a_persisted_reservation() throws Exception {
+
+        LOGGER.info("Running insert_should_return_a_persisted_reservation");
+
+        // Given
+        String request = "{\"passengers\":[{\"firstName\":\"Jose\",\"lastName\":\"Sacco\",\"documentNumber\":\"AB554713\",\"documentType\":\"PASSPORT\",\"birthday\":\"1985-01-01\"}],\"itinerary\":{\"segment\":[{\"origin\":\"BUE\",\"destination\":\"MIA\",\"departure\":\"2023-12-31\",\"arrival\":\"2024-01-01\",\"carrier\":\"AA\"}],\"price\":{\"totalPrice\":30.00,\"totalTax\":20.00,\"basePrice\":10.00}},\"creationDate\":\"2023-11-11\"}";
+
+        // When
+        MvcResult mvcResult = mockMvc.perform(post("/reservation")
+                        .content(request).contentType(CONTENT_TYPE))
+                .andReturn();
+        // Then
+        assertAll(
+                () -> assertEquals(201, mvcResult.getResponse().getStatus()),
+                () -> assertEquals(CONTENT_TYPE, mvcResult.getResponse().getContentType()),
+                () -> assertTrue(mvcResult.getResponse().getContentAsString().contains("\"departure\":\"2023-12-31\""))
+        );
+    }
+
+    @Order(3)
+    @Tag("success-case")
+    @DisplayName("should update a persisted reservation")
+    @Test
+    void update_should_change_a_persisted_reservation() throws Exception {
+
+        LOGGER.info("Running update_should_change_a_persisted_reservation");
+
+        // Given
+        String requestBody = "{\"id\":4,\"version\":2,\"passengers\":[{\"firstName\":\"Jose\",\"lastName\":\"Sacco\",\"documentNumber\":\"AB554713\",\"documentType\":\"PASSPORT\",\"birthday\":\"1985-01-01\"},{\"firstName\":\"Andres\",\"lastName\":\"Sacco\",\"documentNumber\":\"AB554718\",\"documentType\":\"PASSPORT\",\"birthday\":\"1985-01-01\"}],\"itinerary\":{\"id\":4,\"version\":2,\"segment\":[{\"origin\":\"BUE\",\"destination\":\"MIA\",\"departure\":\"2023-12-31\",\"arrival\":\"2024-01-01\",\"carrier\":\"AA\"}],\"price\":{\"totalPrice\":30.00,\"totalTax\":20.00,\"basePrice\":10.00}},\"creationDate\":\"2023-11-11\"}";
+        int requestId = 4;
+
+        // When
+        MvcResult mvcResult = mockMvc.perform(put("/reservation/".concat(String.valueOf(requestId)))
+                        .content(requestBody)
+                        .contentType(CONTENT_TYPE))
+                .andReturn();
+        // Then
+        assertAll(
+                () -> assertEquals(200, mvcResult.getResponse().getStatus()),
+                () -> assertEquals(CONTENT_TYPE, mvcResult.getResponse().getContentType()),
+                () -> assertTrue(mvcResult.getResponse().getContentAsString().contains("\"firstName\":\"Jose\""))
+        );
+    }
+
+
+
+    @Order(4)
+    @Tag("success-case")
+    @DisplayName("should remove a persisted reservation")
+    @Test
+    void delete_should_remove_a_persisted_reservation() throws Exception {
+
+        LOGGER.info("Running delete_should_remove_a_persisted_reservation");
+
+        // Given
+        int request = 10;
+
+        // When
+        MvcResult mvcResult = mockMvc.perform(delete("/reservation/".concat(String.valueOf(request))).contentType(CONTENT_TYPE))
+                .andReturn();
+        // Then
+        assertAll(
+                () -> assertEquals(200, mvcResult.getResponse().getStatus())
+        );
+    }
+
 
 
     @Tag("error-case")
