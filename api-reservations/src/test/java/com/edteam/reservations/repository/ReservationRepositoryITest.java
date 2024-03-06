0268e6c3f7a2bae5e279782392ab96011148d8c3
asacco <andres.sacco@travelx.io>
2024-03-06 17:22:26 -0300

    final

---
+++ api-reservations/src/test/java/com/edteam/reservations/repository/ReservationRepositoryITest.java
@@ -0,0 +1,92 @@
+package com.edteam.reservations.repository;
+
+import com.edteam.reservations.model.Reservation;
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.MountableFile;
+
+import java.util.List;
+
+import static com.edteam.reservations.util.ReservationUtil.getReservation;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Tags(@Tag("integration"))
+@DisplayName("Check the functionality of the repository")
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ReservationRepositoryITest {
+
+    @Autowired
+    ReservationRepository repository;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReservationRepositoryITest.class);
+
+    public static MySQLContainer container = new MySQLContainer<>("mysql:8.2.0").withUsername("root")
+            .withPassword("muppet").withDatabaseName("flights_reservation")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("/db/init.sql"),
+                    "/docker-entrypoint-initdb.d/schema.sql")
+            .withReuse(true);
+
+
+    @BeforeAll
+    public static void setUp() {
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
+
+    @Tag("success-case")
+    @DisplayName("should return the information of all the reservations")
+    @Test
+    void getReservations_should_return_the_information() {
+        LOGGER.info("Running getReservations_should_return_the_information");
+
+        // When
+        List<Reservation> result = repository.findAll();
+
+        // Then
+        assertAll(
+                () -> assertNotNull(result),
+                () -> assertThat(result, hasSize(5)),
+                () -> assertEquals(getReservation(1L, "BUE", "MIA").toString(), result.get(0).toString()),
+                () -> assertThat(result.get(0), hasProperty("id")),
+                () -> assertThat(result.get(0).getPassengers().get(0).getFirstName(), stringContainsInOrder("A", "s")),
+                () -> assertThat(result.get(0).getPassengers().get(0).getFirstName(), matchesRegex("[a-zA-Z]+")));
+    }
+
+    @Tag("success-case")
+    @DisplayName("should return the information of all the reservations")
+    @Test
+    void save_should_return_the_information() {
+
+        LOGGER.info("Running save_should_return_the_information");
+
+        // Given
+        String origin = "BUE";
+        String destination = "MIA";
+
+        // When
+        Reservation reservation = repository.save(getReservation(null, origin, destination));
+
+        // Then
+        assertAll(() -> assertNotNull(reservation),
+                () -> assertEquals(origin, reservation.getItinerary().getSegment().get(0).getOrigin()),
+                () -> assertEquals(destination, reservation.getItinerary().getSegment().get(0).getDestination()));
+    }
+}
---
+++ api-reservations/src/test/java/com/edteam/reservations/util/ReservationUtil.java
@@ -0,0 +1,80 @@
+package com.edteam.reservations.util;
+
+import com.edteam.reservations.dto.*;
+import com.edteam.reservations.model.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+public class ReservationUtil {
+
+    public static ReservationDTO getReservationDTO(Long id, String origin, String destination) {
+        PassengerDTO passenger = new PassengerDTO();
+        passenger.setFirstName("Andres");
+        passenger.setLastName("Sacco");
+        passenger.setDocumentType("DNI");
+        passenger.setDocumentNumber("12345678");
+        passenger.setBirthday(LocalDate.of(1985, 1, 1));
+
+        PriceDTO price = new PriceDTO();
+        price.setBasePrice(BigDecimal.ONE);
+        price.setTotalTax(BigDecimal.ZERO);
+        price.setTotalPrice(BigDecimal.ONE);
+
+        SegmentDTO segment = new SegmentDTO();
+        segment.setArrival("2025-01-01");
+        segment.setDeparture("2024-12-31");
+        segment.setOrigin(origin);
+        segment.setDestination(destination);
+        segment.setCarrier("AA");
+
+        ItineraryDTO itinerary = new ItineraryDTO();
+        itinerary.setPrice(price);
+        itinerary.setSegment(List.of(segment));
+
+        ReservationDTO reservation = new ReservationDTO();
+        reservation.setId(id);
+        reservation.setPassengers(List.of(passenger));
+        reservation.setItinerary(itinerary);
+
+        return reservation;
+    }
+
+    public static Reservation getReservation(Long id, String origin, String destination) {
+        Passenger passenger = new Passenger();
+        passenger.setFirstName("Andres");
+        passenger.setLastName("Sacco");
+        passenger.setId(id);
+
+        passenger.setDocumentType("DNI");
+        passenger.setDocumentNumber("12345678");
+        passenger.setBirthday(LocalDate.of(1985, 1, 1));
+
+        Price price = new Price();
+        price.setBasePrice(BigDecimal.ONE);
+        price.setTotalTax(BigDecimal.ZERO);
+        price.setTotalPrice(BigDecimal.ONE);
+
+        Segment segment = new Segment();
+        segment.setArrival("2025-01-01");
+        segment.setDeparture("2024-12-31");
+        segment.setOrigin(origin);
+        segment.setDestination(destination);
+        segment.setCarrier("AA");
+        segment.setId(id);
+
+        Itinerary itinerary = new Itinerary();
+        itinerary.setId(id);
+        itinerary.setPrice(price);
+        itinerary.setSegment(List.of(segment));
+
+        Reservation reservation = new Reservation();
+        reservation.setId(id);
+        reservation.setPassengers(List.of(passenger));
+        reservation.setItinerary(itinerary);
+        reservation.setCreationDate(LocalDate.of(2023,11,11));
+
+        return reservation;
+    }
+}
