2a6f70299ce770b8ab1cbcbcc20468642d4abcd3
asacco <andres.sacco@travelx.io>
2024-03-08 14:41:26 -0300

    fix: solve problems with the name

--- api-reservations/src/test/java/com/edteam/reservations/repository/ReservationRepositoryITest.java
+++ api-reservations/src/test/java/com/edteam/reservations/repository/ReservationRepositoryITest.java
@@ -86,7 +86,7 @@ class ReservationRepositoryITest {
     @ExpectMaxQueryExecutionTime(thresholdInMilliSeconds = 8) // This check the duration of the execution of the query
     @AnalyzeSql
     @Tag("success-case")
-    @DisplayName("should return the information of all the reservations")
+    @DisplayName("should save a reservation")
     @Test
     void save_should_return_the_information() {
 
