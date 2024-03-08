8e016a648a1328f309b38188fae925dcf755782a
asacco <andres.sacco@travelx.io>
2024-03-08 11:51:31 -0300

    fix: solve the problem with the basic tests

--- api-reservations/src/test/java/com/edteam/reservations/ApiReservationsApplicationITest.java
+++ api-reservations/src/test/java/com/edteam/reservations/ApiReservationsApplicationITest.java
@@ -1,11 +1,31 @@
 package com.edteam.reservations;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class ApiReservationsApplicationITest {
 
+    public static MySQLContainer container = new MySQLContainer<>("mysql:8.0").withUsername("root")
+            .withPassword("muppet").withDatabaseName("flights_reservation").withReuse(true);
+
+    @BeforeAll
+    public static void setUp() {
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void sqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
     @Test
     void contextLoad() {
 
